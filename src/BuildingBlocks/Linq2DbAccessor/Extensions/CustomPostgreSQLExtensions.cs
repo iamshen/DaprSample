5c2b30f09822efcd17a391389a71d3dfee456a05
iamshen <lzzyjsj@outlook.com>
2024-04-16 10:53:29 +0800

    Test json column query

--- src/BuildingBlocks/Linq2DbAccessor/Extensions/CustomPostgreSQLExtensions.cs
+++ src/BuildingBlocks/Linq2DbAccessor/Extensions/CustomPostgreSQLExtensions.cs
@@ -1,86 +1,86 @@
-﻿using System.Linq.Expressions;
-#nullable disable
+﻿#nullable disable
 
-namespace LinqToDB
+using System.Linq.Expressions;
+
+namespace LinqToDB;
+
+/// <summary>
+///     PostgreSQL 自定义查询函数扩展
+/// </summary>
+public static class CustomPostgreSQLExtensions
 {
     /// <summary>
-    /// Postgre SQL 自定义查询函数扩展
+    ///     查询实体中 jsonb/json 字段中的某个属性(仅限字符串)
+    ///     <example>
+    ///         <para>例如：</para>
+    ///         <code>
+    ///             var query = db.XXX.WhereIf(x => x.Authentication.JsonExtractPathText(json => json.RealName) == "张三" );
+    ///         </code>
+    ///         <para>可以参考 https://github.com/linq2db/linq2db/blob/master/Tests/Linq/Linq/ExpressionsTests.cs </para>
+    ///     </example>
     /// </summary>
-    public static class CustomPostgreSQLExtensions
+    /// <typeparam name="TColumn">查询列</typeparam>
+    /// <typeparam name="TJsonProp">查询的属性</typeparam>
+    /// <param name="field">字段</param>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    [ExpressionMethod(nameof(JsonExtractPathExpression))]
+    public static TJsonProp JsonExtractPathText<TColumn, TJsonProp>(this TColumn field,
+        Expression<Func<TColumn, TJsonProp>> path) => throw new InvalidOperationException();
+
+    /// <summary>
+    ///     表达式转换
+    /// </summary>
+    /// <typeparam name="TColumn"></typeparam>
+    /// <typeparam name="TJsonProp"></typeparam>
+    /// <returns></returns>
+    private static Expression<Func<TColumn, Expression<Func<TColumn, TJsonProp>>, TJsonProp>>
+        JsonExtractPathExpression<TColumn, TJsonProp>()
     {
-        /// <summary>
-        /// 查询实体中 jsonb/json 字段中的某个属性
-        /// <example>
-        /// <para>例如：</para>
-        /// <code>
-        ///     var query = db.XXX.WhereIf(x => x.Authentication.JsonExtractPathText(json => json.RealName) == "张三" );
-        /// </code>
-        /// <para>可以参考 https://github.com/linq2db/linq2db/blob/master/Tests/Linq/Linq/ExpressionsTests.cs </para>
-        /// </example>
-        /// </summary>
-        /// <typeparam name="TColumn">查询列</typeparam>
-        /// <typeparam name="TJsonProp">查询的属性</typeparam>
-        /// <param name="field">字段</param>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        [ExpressionMethod(nameof(JsonExtractPathExpression))]
-        public static TJsonProp JsonExtractPathText<TColumn, TJsonProp>(this TColumn field, Expression<Func<TColumn, TJsonProp>> path)
+        return (column, jsonProp)
+            => JsonExtractPathText<TColumn, TJsonProp>(column, Sql.Expr<string>(JsonPath(jsonProp)));
+    }
+
+    [Sql.Expression("{0}::json #>> {1}", ServerSideOnly = true, IsPredicate = true)]
+    private static TJsonProp JsonExtractPathText<TColumn, TJsonProp>(TColumn left, string right)
+        => throw new InvalidOperationException();
+
+    private static string JsonPath<TColumn, TJsonProp>(Expression<Func<TColumn, TJsonProp>> extractor)
+    {
+        var name = string.Empty;
+        try
         {
-            throw new InvalidOperationException();
+            var expression = GetMemberInfo(extractor);
+            name = expression.Member.Name;
+        }
+        catch
+        {
         }
 
-        /// <summary>
-        /// 表达式转换
-        /// </summary>
-        /// <typeparam name="TColumn"></typeparam>
-        /// <typeparam name="TJsonProp"></typeparam>
-        /// <returns></returns>
-        private static Expression<Func<TColumn, Expression<Func<TColumn, TJsonProp>>, TJsonProp>> JsonExtractPathExpression<TColumn, TJsonProp>()
+        return "'{" + name + "}'";
+    }
+
+    private static MemberExpression GetMemberInfo(Expression method)
+    {
+        if (method is not LambdaExpression lambda)
+            throw new ArgumentNullException(nameof(method));
+
+        MemberExpression memberExpr = null;
+
+        switch (lambda.Body.NodeType)
         {
-            return (column, jsonProp) => JsonExtractPathText<TColumn, TJsonProp>(column, Sql.Expr<string>(JsonPath(jsonProp)));
+            case ExpressionType.Convert:
+                memberExpr = ((UnaryExpression)lambda.Body).Operand as MemberExpression;
+                break;
+
+            case ExpressionType.MemberAccess:
+                memberExpr = lambda.Body as MemberExpression;
+                break;
         }
 
-        [Sql.Expression("{0}::json #>> {1}", ServerSideOnly = true, IsPredicate = true)]
-        private static TJsonProp JsonExtractPathText<TColumn, TJsonProp>(TColumn left, string right)
-        {
-            throw new InvalidOperationException();
-        }
+        if (memberExpr is null)
+            throw new ArgumentException(nameof(memberExpr));
 
-        private static string JsonPath<TColumn, TJsonProp>(Expression<Func<TColumn, TJsonProp>> extractor)
-        {
-            string name = string.Empty;
-            try
-            {
-                var expression = GetMemberInfo(extractor);
-                name = expression.Member.Name;
-            }
-            catch
-            { }
-            return "'{" + name + "}'";
-        }
-
-        private static MemberExpression GetMemberInfo(Expression method)
-        {
-            if (method is not LambdaExpression lambda)
-                throw new ArgumentNullException(nameof(method));
-
-            MemberExpression memberExpr = null;
-
-            switch (lambda.Body.NodeType)
-            {
-                case ExpressionType.Convert:
-                    memberExpr = ((UnaryExpression)lambda.Body).Operand as MemberExpression;
-                    break;
-
-                case ExpressionType.MemberAccess:
-                    memberExpr = lambda.Body as MemberExpression;
-                    break;
-            }
-
-            if (memberExpr is null)
-                throw new ArgumentException(nameof(memberExpr));
-
-            return memberExpr;
-        }
+        return memberExpr;
     }
 }
---
+++ test/DaprTool.AbstractionsTest/DataBases/JsonQueryTest.cs
@@ -0,0 +1,115 @@
+﻿using DaprTool.AbstractionsTest.Base;
+using DaprTool.BuildingBlocks.Linq2DbAccessor.Converter;
+using DaprTool.BuildingBlocks.Linq2DbAccessor.Data;
+using LinqToDB;
+using LinqToDB.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit.Abstractions;
+
+namespace DaprTool.AbstractionsTest.DataBases;
+
+public class JsonQueryTest(DbDependencySetupFixture fixture, ITestOutputHelper testOutputHelper)
+    : IClassFixture<DbDependencySetupFixture>
+{
+    [Fact(DisplayName = "01. JsonWhere")]
+    public async Task Test1()
+    {
+        using var scope = fixture.ServiceProvider.CreateScope();
+        await using var db = scope.ServiceProvider.GetAppConnection();
+
+        await db.DropTableAsync<MyJsonTable>(throwExceptionIfNotExists: false);
+        var table = db.CreateTable<MyJsonTable>();
+        Assert.NotNull(table);
+
+        var affectedRow = await db.InsertAsync(new MyJsonTable
+        {
+            Id = Guid.NewGuid().ToString(),
+            Code = Guid.NewGuid().ToString("N"),
+            Remark = "testing",
+            CapitalAccount = new CapitalAccount(723453551573140001, "a", Random.Shared.NextDouble()),
+            CertificationInfo = new CertificationInfo("张三", "110123...", "18888888881")
+        });
+        Assert.True(affectedRow > 0);
+        var affectedRow1 = await db.InsertAsync(new MyJsonTable
+        {
+            Id = Guid.NewGuid().ToString(),
+            Code = Guid.NewGuid().ToString("N"),
+            Remark = "testing1",
+            CapitalAccount = new CapitalAccount(723453551573140002, "b", Random.Shared.NextDouble()),
+            CertificationInfo = new CertificationInfo("李四", "110456...", "18888888882")
+        });
+        Assert.True(affectedRow1 > 0);
+
+        var query = db.GetTable<MyJsonTable>()
+            .Where(x => x.CapitalAccount != null)
+            .JsonWhere(x => x.CapitalAccount.CapitalId == 723453551573140002);
+        testOutputHelper.WriteLine($"查询 sql：{query}");
+
+        var row = await query.FirstOrDefaultAsync();
+        Assert.NotNull(row);
+
+        testOutputHelper.WriteLine($"查询结果：{row}");
+    }
+
+    [Fact(DisplayName = "02. JsonExtractPathText")]
+    public async Task Test2()
+    {
+        using var scope = fixture.ServiceProvider.CreateScope();
+        await using var db = scope.ServiceProvider.GetAppConnection();
+
+        await db.DropTableAsync<MyJsonTable>(throwExceptionIfNotExists: false);
+        var table = db.CreateTable<MyJsonTable>();
+        Assert.NotNull(table);
+
+        var affectedRow = await db.InsertAsync(new MyJsonTable
+        {
+            Id = Guid.NewGuid().ToString(),
+            Code = Guid.NewGuid().ToString("N"),
+            Remark = "testing",
+            CapitalAccount = new CapitalAccount(723453551573140001, "a", Random.Shared.NextDouble()),
+            CertificationInfo = new CertificationInfo("张三", "110123...", "18888888881")
+        });
+        Assert.True(affectedRow > 0);
+        var affectedRow1 = await db.InsertAsync(new MyJsonTable
+        {
+            Id = Guid.NewGuid().ToString(),
+            Code = Guid.NewGuid().ToString("N"),
+            Remark = "testing1",
+            CapitalAccount = new CapitalAccount(723453551573140002, "b", Random.Shared.NextDouble()),
+            CertificationInfo = new CertificationInfo("李四", "110456...", "18888888882")
+        });
+        Assert.True(affectedRow1 > 0);
+
+        var query = db.GetTable<MyJsonTable>()
+            .Where(x => x.CapitalAccount != null)
+            .Where(x => x.CertificationInfo.JsonExtractPathText(json => json.RealName) == "张三");
+        testOutputHelper.WriteLine($"查询 sql：{query}");
+
+        var row = await query.FirstOrDefaultAsync();
+        Assert.NotNull(row);
+
+        testOutputHelper.WriteLine($"查询结果：{row}");
+    }
+}
+
+[Table(Schema = "public", Name = "t_testing_json_table")]
+public class MyJsonTable : EntityBase
+{
+    [Column("code", CanBeNull = false)] public string Code { get; set; } = string.Empty;
+
+    [Column("remark", CanBeNull = true)] public string? Remark { get; set; } = string.Empty;
+
+    [Column("capital_account", DataType = DataType.BinaryJson, CanBeNull = true)]
+    [ValueConverter(ConverterType = typeof(JsonValueConverter<CapitalAccount>))]
+    public CapitalAccount? CapitalAccount { get; set; }
+
+    [Column("certification_info", DataType = DataType.BinaryJson, CanBeNull = true)]
+    [ValueConverter(ConverterType = typeof(JsonValueConverter<CertificationInfo>))]
+    public CertificationInfo? CertificationInfo { get; set; }
+
+    public override string ToString() => Code + Remark;
+}
+
+public record CapitalAccount(long CapitalId, string Account, double Balance);
+
+public record CertificationInfo(string RealName, string IdCard, string PhoneNumber);
