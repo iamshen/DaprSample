ada83668ba655dec07718440de100e1763ed13f3
iamshen <lzzyjsj@outlook.com>
2024-05-16 09:52:44 +0800

    Update Identity Claim JsonMapAction

---
+++ src/ProxyServer/TransformProviders/DaprTransformProvider.cs
@@ -0,0 +1,58 @@
+﻿using DaprTool.BuildingBlocks.Utils.Constant;
+using Yarp.ReverseProxy.Forwarder;
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Transforms.Builder;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public class DaprTransformProvider(ILogger<DaprTransformProvider> logger) : ITransformProvider
+{
+    public void Apply(TransformBuilderContext context)
+    {
+        var route = ApplicationConstants.AllRoutes.FirstOrDefault(x => x.AppId.Equals(context.Route.RouteId, StringComparison.OrdinalIgnoreCase));
+        if (route is not null)
+        {
+            context.AddRequestTransform((RequestTransformContext transformContext) =>
+            {
+                string catchAll = string.Empty;
+                var requestPath = transformContext.Path.Value!;
+
+                if (string.IsNullOrWhiteSpace(route.BasePath) && requestPath.StartsWith(ApplicationConstants.ApiPathPrefix))
+                {
+                    catchAll = requestPath[$"{ApplicationConstants.ApiPathPrefix}/{route.AppId}".Length..];
+                }
+                else
+                {
+                    catchAll = requestPath;
+                }
+
+                var queryContext = new QueryTransformContext(transformContext.HttpContext.Request);
+
+                var newPathUri = RequestUtilities.MakeDestinationAddress(
+                    transformContext.DestinationPrefix,
+                    new PathString(string.Format(ApplicationConstants.DaprServiceInvocation, context.Route.RouteId, catchAll)),
+                    queryContext.QueryString);
+
+                logger.LogInformation("proxy to new path uri: {0}", newPathUri);
+
+                transformContext.ProxyRequest.RequestUri = newPathUri;
+
+                return ValueTask.CompletedTask;
+            });
+        }
+    }
+
+    public void ValidateCluster(TransformClusterValidationContext context)
+    {
+        /*if (context.Cluster.ClusterId== DaprConfigUtils.DaprApiClusterId)
+        {
+        }*/
+    }
+
+    public void ValidateRoute(TransformRouteValidationContext context)
+    {
+        /*if (context.Route.RouteId == DaprConfigUtils.DaprApiRouteId)
+        {
+        }*/
+    }
+}
