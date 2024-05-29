174cefb71614de7be139ab9eca8844316560aa32
iamshen <lzzyjsj@outlook.com>
2024-05-29 09:35:44 +0800

    Upgrade dotnet aspire

--- src/ProxyServer/Program.cs
+++ src/ProxyServer/Program.cs
@@ -1,4 +1,15 @@
+using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+using var serilog = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(serilog, dispose: false);
 
 builder.AddServiceDefaults();
 
@@ -9,7 +20,6 @@ builder.WebHost.ConfigureKestrel(options =>
 {
     options.Limits.MaxResponseBufferSize = 131072; // 增加到 128KB，根据需要调整
 });
-
 
 // 如果不知道 路由 匹配 match 怎么配置 请参考链接
 // https://learn.microsoft.com/zh-cn/aspnet/core/fundamentals/routing?view=aspnetcore-8.0#route-templates
