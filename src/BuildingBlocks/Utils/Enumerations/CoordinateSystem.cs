6239aa6a6de6f6b388f91a4eacb409c4ff3e4e36
iamshen <lzzyjsj@outlook.com>
2024-12-16 09:58:12 +0800

    Adding a Coordinate System Enumeration

---
+++ src/BuildingBlocks/Utils/Enumerations/CoordinateSystem.cs
@@ -0,0 +1,21 @@
+﻿namespace DaprTool.BuildingBlocks.Utils.Enumerations;
+
+/// <summary>
+/// 地理空间参考系统（Spatial Reference System, SRS），
+/// 它定义了如何将地球上的位置映射到二维或三维坐标。
+/// 不同的应用和国家可能会使用不同的坐标系，
+/// 下面是一些常见的坐标系
+/// </summary>
+public enum CoordinateSystem
+{
+    /// <summary>WGS 84坐标系</summary>
+    WGS84,
+    /// <summary>GCJ-02坐标系</summary>
+    GCJ02,
+    /// <summary>BD-09坐标系</summary>
+    BD09,
+    /// <summary>EPSG:3857坐标系</summary>
+    EPSG3857,
+    /// <summary>CGCS2000坐标系</summary>
+    CGCS2000,
+}
